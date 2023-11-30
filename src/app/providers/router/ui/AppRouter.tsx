c7d33757f038240741f1d09f920845d2cea2e741
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2023-11-30 11:43:52 +0300

    add babel, add extract plugin

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,7 +1,5 @@
 import React, {Suspense} from 'react';
 import {Route, Routes} from 'react-router-dom';
-import {AboutPage} from 'pages/AboutPage';
-import {MainPage} from 'pages/MainPage';
 import {routeConfig} from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => {
@@ -9,7 +7,12 @@ const AppRouter = () => {
         <Suspense fallback={<>Loading...</>}>
             <Routes>
                 {Object.values(routeConfig).map(({element, path}) => (
-                    <Route key={path} path={path} element={element}/>
+                    <Route key={path} path={path} element={
+                        <div className="page-wrapper">
+                            {element}
+                        </div>
+
+                    }/>
                 ))}
             </Routes>
         </Suspense>
